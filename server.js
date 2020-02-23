c72f232aef839cbf2ddd7e128c59c15ee64d7e3a
Alexander Trout <alexandertrout@A1.local>
2020-02-23 13:36:48 +0000

    prep heroku files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const path = require( 'path' );
+const cors = require("cors");
 const apiRouter = require('./routers/api-router')
 const router = express.Router();
 const app = express();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( bodyParser.json() );
+app.use(cors());
 app.use('/api', apiRouter);
 
 if ( process.env.NODE_ENV === 'production' ) {
