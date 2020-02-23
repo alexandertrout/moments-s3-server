adbc0cdc210f539105638863db33adfa5329891a
Alexander Trout <alexandertrout@A1.local>
2020-02-23 14:10:34 +0000

    added get endpoint

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,6 +1,8 @@
 const apiRouter = require("express").Router();
 const imageRouter = require('./image-router');
 
+
+apiRouter.get("/", (req, res, next) => res.send({ msg: "Just the first endpoint" }));
 apiRouter.use("/images", imageRouter);
 
 module.exports = apiRouter;
