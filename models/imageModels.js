46bd8ddaa5e86f7647c38762719db169a16eb557
Alexander Trout <alexandertrout@A1.local>
2020-02-27 15:21:08 +0000

    updated

--- models/imageModels.js
+++ models/imageModels.js
@@ -2,7 +2,7 @@ const aws = require("aws-sdk");
 const multerS3 = require("multer-s3");
 const multer = require("multer");
 const path = require("path");
-const fileFilter = require(".");
+const fileFilter = require("../utils/utils");
 
 const S3_BUCKET = process.env.S3_BUCKET;
 
