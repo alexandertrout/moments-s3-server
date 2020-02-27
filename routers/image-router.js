d516bf5e0dc5318bd76bda7149aed3f8d3544778
Alexander Trout <alexandertrout@A1.local>
2020-02-27 14:53:41 +0000

    updated

--- routers/image-router.js
+++ routers/image-router.js
@@ -1,6 +1,6 @@
 const imageRouter = require("express").Router();
 const { uploadImage}  = require("../controllers/imageControllers")
-const handle405 = require("../errors");
+const {handle405} = require("../errors");
 
 imageRouter.route("/").post(uploadImage);
 
