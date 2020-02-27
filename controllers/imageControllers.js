5a88dd52ea7836ecf808282c708b050564288c2a
Alexander Trout <alexandertrout@A1.local>
2020-02-27 14:58:22 +0000

    updated

--- controllers/imageControllers.js
+++ controllers/imageControllers.js
@@ -1,21 +1,19 @@
 const { postImage } = require('../models/imageModels');
 
 exports.uploadImage = (req, res, next) => {
-
+return new Promise((resolve, reject) => {
 	postImage(req, res, ( error ) => {
 		if( error ){
-			res.json( { error: error } );
+			reject(error);
 		} else {
-			if( req.file === undefined ){
-				res.json( 'Error: No File Selected' );
-			} else {
 				const imageName = req.file.key;
         const imageLocation = req.file.location;
 				res.json({
 					image: imageName,
 					location: imageLocation
-        });
+				});
+				resolve();
 			}
-		}
-	})
+		})
+	}).catch(next);
 };
