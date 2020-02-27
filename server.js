857c970f7f8a2cd4e109994348539ae63a08a7a2
Alexander Trout <alexandertrout@A1.local>
2020-02-27 14:40:12 +0000

    updated

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
-const path = require( 'path' );
 const cors = require("cors");
 const apiRouter = require('./routers/api-router')
 const router = express.Router();
@@ -12,14 +11,6 @@ app.use( bodyParser.json() );
 app.use(cors());
 app.use('/api', apiRouter);
 
-if ( process.env.NODE_ENV === 'production' ) {
-	// Set a static folder
-	app.use( express.static( 'client/build' ) );
-	app.get( '*', ( req, res ) => res.sendFile( path.resolve( __dirname, 'client', 'build', 'index.html' ) ) );
-}
+app.listen( PORT ) ;
 
-//comment
-
-app.listen( PORT, () => console.log( `Server running on port: ${PORT}` ) );
-
-module.exports = router;
+module.exports = { router, app }
